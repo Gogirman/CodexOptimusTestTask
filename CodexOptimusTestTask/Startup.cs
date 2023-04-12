c49379b5df357591b13450b812549f6c603edc60
George Shipitcin <gogirman111@gmail.com>
2023-04-12 05:31:39 +0500

    Main commit

---
+++ CodexOptimusTestTask/Startup.cs
@@ -0,0 +1,58 @@
+using CodexOptimusTestTask.Models.Dtos;
+using CodexOptimusTestTask.Services;
+using CodexOptimusTestTask.Validators;
+using FluentValidation;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Microsoft.OpenApi.Models;
+
+namespace CodexOptimusTestTask
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddControllers();
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = "CodexOptimusTestTask", Version = "v1" });
+            });
+
+            services.AddScoped<ILoanPaymentsService, LoanPaymentsService>();
+            services.AddScoped<IValidator<GetLoanPaymentsScheduleDto>, GetPaymentScheduleValidator>();
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "CodexOptimusTestTask v1"));
+            }
+
+            app.UseHttpsRedirection();
+
+            app.UseRouting();
+
+            app.UseAuthorization();
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllers();
+            });
+        }
+    }
+}
---
+++ README.md
@@ -0,0 +1,2 @@
+# CodexOptimusTestTask
+ 
